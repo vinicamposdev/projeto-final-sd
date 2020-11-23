6aa73dbe018cc441818ba0e43b900d13d242a71a
Israel Moraes <israelfmoraes@outlook.com>
2020-11-23 19:52:07 -0300

    refactor: remove interval from subscribeQueue

--- producer-humidity/src/producer/humidity.js
+++ producer-humidity/src/producer/humidity.js
@@ -1,36 +1,36 @@
 const service = require('../service/rabbitmq')
 const mailer = require('../service/mailer');
 
-const getRandom = (min,max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+const getRandom = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 async function connectToRabbit() {
-  
-  const exchange = 'metrics'
 
-  const message = {
-    type: 'humidity',
-    humidity: getRandom(-150,300),
-    time: new Date()
-  }
-  await service.sendMessage(exchange,'humidity',message)
+    const exchange = 'metrics'
 
-  let invalidMessages = [];
-  setInterval(async () => {
+    const message = {
+        type: 'humidity',
+        humidity: getRandom(-150, 300),
+        time: new Date()
+    }
+    await service.sendMessage(exchange, 'humidity', message)
+
+    let invalidMessages = [];
     await service.subscribeQueue('invalid_data', function (messageRaw) {
         invalidMessages.push(messageRaw);
     });
 
-    if (invalidMessages.length > 0) {
-        mailer.sendMail(JSON.stringify(invalidMessages));
-        invalidMessages = [];
-    }
-  }, 5000);
+    setInterval(async () => {
+        if (invalidMessages.length > 0) {
+            mailer.sendMail(JSON.stringify(invalidMessages));
+            invalidMessages = [];
+        }
+    }, 5000);
 }
 
-async function handle () {
-  setInterval(connectToRabbit, 100);
+async function handle() {
+    setInterval(connectToRabbit, 100);
 }
 
 handle()
